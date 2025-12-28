71f823cf792837e29dba6186534b942168957d61
abilioneto <abilio@bsd.com.br>
2025-12-28 14:08:40 -0300

    doc rest api with Spring rest docs

--- src/test/java/com/algaworks/algashop/product/catalog/contract/base/ProductBase.java
+++ src/test/java/com/algaworks/algashop/product/catalog/contract/base/ProductBase.java
@@ -1,18 +1,24 @@
 package com.algaworks.algashop.product.catalog.contract.base;
 
+import com.algaworks.algashop.product.catalog.application.PageModel;
 import com.algaworks.algashop.product.catalog.application.ResourceNotFoundException;
 import com.algaworks.algashop.product.catalog.application.product.management.ProductInput;
 import com.algaworks.algashop.product.catalog.application.product.management.ProductManagementApplicationService;
-import com.algaworks.algashop.product.catalog.application.PageModel;
 import com.algaworks.algashop.product.catalog.application.product.query.ProductDetailOutput;
 import com.algaworks.algashop.product.catalog.application.product.query.ProductDetailOutputTestDataBuilder;
 import com.algaworks.algashop.product.catalog.application.product.query.ProductQueryService;
 import com.algaworks.algashop.product.catalog.presentation.ProductController;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.restdocs.operation.preprocess.Preprocessors;
+import org.springframework.restdocs.templates.TemplateFormats;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -21,7 +27,10 @@ import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.UUID;
 
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+
 @WebMvcTest(controllers = ProductController.class)
+@ExtendWith(RestDocumentationExtension.class)
 public class ProductBase {
 
     @Autowired
@@ -40,9 +49,16 @@ public class ProductBase {
     public static final UUID createdProductId = UUID.fromString("f7c6843f-465c-476d-9a9b-4783bde4dc5e");
 
     @BeforeEach
-    void setUp() {
+    void setUp(RestDocumentationContextProvider documentationContextProvider) {
         RestAssuredMockMvc.mockMvc(MockMvcBuilders.webAppContextSetup(context)
-                .defaultResponseCharacterEncoding(StandardCharsets.UTF_8).build());
+                .apply(documentationConfiguration(documentationContextProvider)
+                        .snippets().withTemplateFormat(TemplateFormats.asciidoctor())
+                        .and().operationPreprocessors()
+                        .withResponseDefaults(Preprocessors.prettyPrint()))
+                .alwaysDo(MockMvcRestDocumentation.document("{ClassName}/{methodName}"))
+                .defaultResponseCharacterEncoding(StandardCharsets.UTF_8)
+                .build()
+        );
 
         RestAssuredMockMvc.enableLoggingOfRequestAndResponseIfValidationFails();
 
